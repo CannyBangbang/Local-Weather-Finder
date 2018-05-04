2ed1886de479390317b8aeda671865cc87b33c95
Brock Lubecker <brocklubecker@gmail.com>
2018-05-04 17:10:13 -0500

    fixed issue with degree symbol not displaying correctly

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
      var pullLatitude;
      var pullLongitude;
      var finalURL;
-    
+
      if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
                pullLatitude = position.coords.latitude;
@@ -10,35 +10,37 @@ $(document).ready(function() {
                pullWeatherAPI(pullLatitude, pullLongitude);
                console.log(pullLatitude, pullLongitude); //check for lat/long as sometimes shows default location in JP
      });
-          };  
-     
+          };
+
      function pullWeatherAPI(pullLatitude, pullLongitude) {
           finalURL = 'https://fcc-weather-api.glitch.me/api/current?lat=' + pullLatitude + '&lon=' + pullLongitude;
           console.log(finalURL); //check for URL as sometimes shows default location in JP
           $.getJSON(finalURL, function weatherFinder(response) {
                $("#weather").html(response.weather[0].main);
-             
+
                var temperatureC = Math.round(response.main.temp)
                var temperatureF = Math.round((response.main.temp * 1.8) + 32)
-               
-               
-               $("#temperature").html(temperatureF + "°"); 
-               $("#celOrFah").html("F");  
-               
+
+
+               $("#temperature").html(temperatureF + "°");
+               $("#celOrFah").html("F");
+
                $("#temp-CelOrFar").click(function () {
                      $("#celOrFah").text(($("#celOrFah").text() == 'F') ? 'C' : 'F');
                      var cOrFSwap = $("#celOrFah").text();
                      if (cOrFSwap == 'F') {
-                        $("#temperature").html(temperatureF + "°"); 
+                        $("#temperature").html(temperatureF + "°");
                      }
                      else if (cOrFSwap == 'C') {
-                        $("#temperature").html(temperatureC + "°"); 
+                        $("#temperature").html(temperatureC + "°");
                      };
                })
-             
-             
-             
-             
+
+               console.log(temperatureF); //test as "64Â°" is showing for a current temp
+               console.log(temperatureC); //test as "64Â°" is showing for a current temp
+
+
+
                $("#city").html(response.name);
                $("#country").html(response.sys.country);
                if (response.weather[0]) {
@@ -76,9 +78,8 @@ $(document).ready(function() {
                         $('#icon-switch').attr('src','https://image.flaticon.com/icons/svg/777/777629.svg');
                         break;
                  };
-               };   
+               };
           });
      };
-     
-});
 
+});
